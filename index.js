1ba2394d79595f03446cc414f7803281fc66e300
kaerhae <erno.karha@hotmail.com>
2021-01-18 22:44:41 +0200

    Added build

--- index.js
+++ index.js
@@ -125,7 +125,7 @@ app.delete('/api/persons/:id', (req, res) => {
     res.status(204).end()
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3002
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
